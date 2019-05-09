4f26a3ee6fe36118d125eab886e5bdc9ee04f1dc
ramyakrishna25 <sramya.krishna28@gmail.com>
2019-05-09 11:02:25 +0530

    commit

---
+++ app/src/main/java/com/sample/currentlocation/MainActivity.java
@@ -0,0 +1,103 @@
+package com.sample.currentlocation;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnGPSShowLocation;
+    Button btnNWShowLocation;
+
+    AppLocationService appLocationService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        appLocationService = new AppLocationService(
+                MainActivity.this);
+
+        btnGPSShowLocation = (Button) findViewById(R.id.btnGPSShowLocation);
+        btnGPSShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Location gpsLocation = appLocationService.getLocation(LocationManager.GPS_PROVIDER);
+
+                if (gpsLocation != null) {
+                    double latitude = gpsLocation.getLatitude();
+                    double longitude = gpsLocation.getLongitude();
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Mobile Location (GPS): \nLatitude: " + latitude
+                                    + "\nLongitude: " + longitude,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                   // showSettingsAlert("GPS");
+                }
+
+            }
+        });
+
+        btnNWShowLocation = (Button) findViewById(R.id.btnNWShowLocation);
+        btnNWShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Location nwLocation = appLocationService
+                        .getLocation(LocationManager.NETWORK_PROVIDER);
+
+                if (nwLocation != null) {
+                    double latitude = nwLocation.getLatitude();
+                    double longitude = nwLocation.getLongitude();
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Mobile Location (NW): \nLatitude: " + latitude
+                                    + "\nLongitude: " + longitude,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                   // showSettingsAlert("NETWORK");
+                }
+
+            }
+        });
+
+    }
+
+    public void showSettingsAlert(String provider) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                MainActivity.this);
+
+        alertDialog.setTitle(provider + " SETTINGS");
+
+        alertDialog
+                .setMessage(provider + " is not enabled! Want to go to settings menu?");
+
+        alertDialog.setPositiveButton("Settings",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        MainActivity.this.startActivity(intent);
+                    }
+                });
+
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialog.show();
+    }
+
+}
